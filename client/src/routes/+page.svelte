8c45b8b0ae363f9c5051500b9ff05399a3eebc6d
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-11-08 21:45:55 +0100

    Completed Image Carousel Selection mock

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { onMount, tick } from 'svelte';
   import { menuListStore, setMenuList } from './../stores/menuList';
   import { themeStore } from './../stores/themeStore';
   import { browser } from '$app/environment';
@@ -10,55 +10,75 @@
     { label: 'Nike', value: 'NIKE', imageUrl: '/img/divisions/nike.jpg' }
   ];
 
-  let selectedBrand = brands[0]; // Initialize with a default brand
+  let selectedBrandIndex = 0; // Use an index to track the selected brand
+  let selectedBrand = brands[selectedBrandIndex];
 
-  // Function to update brand and theme
-  function nextBrand() {
-    const currentIndex = brands.findIndex(brand => brand.value === selectedBrand.value);
-    const nextIndex = (currentIndex + 1) % brands.length;
-    selectedBrand = brands[nextIndex];
-    setMenuList(selectedBrand.value);
-    themeStore.set(selectedBrand.value); // This updates the theme
-    if (browser) {
-      localStorage.setItem('selectedBrand', selectedBrand.value); // Update localStorage on the client-side
-    }
-  }
-
-  // Initialize the menu and theme on component mount, including localStorage handling
+  // Initialize selectedBrand from localStorage or default to the first brand
   onMount(() => {
-    let initialBrandValue = brands[0].value;
-
     if (browser) {
-      // Client-side logic
-      initialBrandValue = localStorage.getItem('selectedBrand') || brands[0].value;
+      const storedBrandValue = localStorage.getItem('selectedBrand');
+      selectedBrandIndex = brands.findIndex(brand => brand.value === storedBrandValue);
+      if (selectedBrandIndex === -1) {
+        selectedBrandIndex = 0; // Fallback to the first brand if not found
+      }
     }
 
-    selectedBrand = brands.find(brand => brand.value === initialBrandValue) || brands[0];
-    setMenuList(selectedBrand.value);
-    themeStore.set(selectedBrand.value);
+    selectedBrand = brands[selectedBrandIndex];
+    updateBrand(selectedBrand);
   });
 
-  // Reactively update the menu and theme when selectedBrand changes
-  $: if (selectedBrand && browser) {
-    setMenuList(selectedBrand.value);
-    themeStore.set(selectedBrand.value);
-    localStorage.setItem('selectedBrand', selectedBrand.value);
+  // Function to handle brand update logic
+  function updateBrand(brand) {
+    setMenuList(brand.value);
+    themeStore.set(brand.value);
+    if (browser) {
+      localStorage.setItem('selectedBrand', brand.value);
+    }
   }
 
-  // Reactive statement to log the current theme
-  $: if ($themeStore) {
-    console.log('Current theme:', $themeStore);
+  // Function to navigate to the next brand
+  function nextBrand() {
+    selectedBrandIndex = (selectedBrandIndex + 1) % brands.length;
+    selectedBrand = brands[selectedBrandIndex];
+    updateBrand(selectedBrand);
+  }
+
+  // Function to navigate to the previous brand
+  function previousBrand() {
+    selectedBrandIndex = (selectedBrandIndex - 1 + brands.length) % brands.length;
+    selectedBrand = brands[selectedBrandIndex];
+    updateBrand(selectedBrand);
   }
 </script>
-    
-<div class="md:container box-border p-4">
-  <p>Another season, another reason to sell !</p>
-  <p>It's time to get rid of all the bugs you don't need anymore.</p>
+
+<div class="carousel-container">
+  <button on:click={previousBrand} class="carousel-button">Previous</button>
+  <img src={selectedBrand.imageUrl} alt={selectedBrand.label} class="carousel-image height-800 width-533"/>
+  <button on:click={nextBrand} class="carousel-button">Next</button>
+
+  <!-- <div class="brand-info">
+    <p>Selected Brand: {selectedBrand.label}</p>
+    <p>Selected Value: {selectedBrand.value}</p>
+    <p>Selected Theme: {$themeStore}</p>
+  </div> -->
 </div>
 
-<div class="image-carousel">
-  <img src={selectedBrand.imageUrl} alt={selectedBrand.label} />
-  <button class="bg-sky-500 hover:bg-sky-700 rounded-xl shadow-lg items-left space-x-4 animate-pulse p-4 my-8" on:click={nextBrand}>Brand</button>
-  <p>Selected Brand: {selectedBrand.label}</p>
-  <p>Selected Theme: {$themeStore}</p>
-</div>
+<style>
+  .carousel-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .carousel-button {
+    padding: 0.5rem 1rem;
+    margin: 0 1rem;
+    cursor: pointer;
+  }
+  .carousel-image {
+    max-width: 100%;
+    height: auto;
+  }
+  .brand-info {
+    text-align: center;
+  }
+</style>
