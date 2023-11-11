858bd9b9e9c9fa692ffa8b492f17cac58aac0c0f
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-11-11 03:44:36 +0100

    basic ccarousel done with nav buttons

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -13,6 +13,25 @@
   let selectedBrandIndex = 0; // Use an index to track the selected brand
   let selectedBrand = brands[selectedBrandIndex];
 
+  // Movie data for each brand (you can fetch this data from an API)
+  let movieData = {
+    'TH': [
+      { title: 'Movie 1', videoUrl: '/FESTIVE MSW1080x1080.mp4' },
+      { title: 'Movie 2', videoUrl: 'https://www.example.com/th-video2.mp4' },
+      // Add more movies for TH brand
+    ],
+    'CK': [
+      { title: 'Movie A', videoUrl: '/Suiting_Desktop.mp4' },
+      { title: 'Movie B', videoUrl: '/Suiting_Desktop.mp4' },
+      // Add more movies for CK brand
+    ],
+    'NIKE': [
+      { title: 'Movie X', videoUrl: '/Underwear_Desktop.mp4' },
+      { title: 'Movie Y', videoUrl: 'https://www.example.com/nike-videoY.mp4' },
+      // Add more movies for NIKE brand
+    ],
+  };
+
   // Initialize selectedBrand from localStorage or default to the first brand
   onMount(() => {
     if (browser) {
@@ -51,34 +70,80 @@
   }
 </script>
 
-<div class="carousel-container">
-  <button on:click={previousBrand} class="carousel-button">Previous</button>
-  <img src={selectedBrand.imageUrl} alt={selectedBrand.label} class="carousel-image height-800 width-533"/>
-  <button on:click={nextBrand} class="carousel-button">Next</button>
+<div class="header-container">
+  <!-- <img src="/img/PVH_Logo.svg" alt="PVH Logo" class="pvh-logo"> -->
+  <h1 class="report-title">DIGITAL SELLING DATA REPORT</h1>
+</div>
 
-  <!-- <div class="brand-info">
-    <p>Selected Brand: {selectedBrand.label}</p>
-    <p>Selected Value: {selectedBrand.value}</p>
-    <p>Selected Theme: {$themeStore}</p>
-  </div> -->
+<div class="carousel-container">
+  <button on:click={previousBrand} class="carousel-button previous">&lt;</button>
+  <div class="video-container">
+    <video src={movieData[selectedBrand.value][0].videoUrl} autoplay loop muted class="carousel-video"></video>
+  </div>
+  <button on:click={nextBrand} class="carousel-button next">&gt;</button>
 </div>
 
 <style>
+
+.header-container {
+  text-align: center; /* Center the contents */
+  padding: 20px; /* Add some padding around the contents */
+  background: #FFF; /* Assuming a white background; adjust as needed */
+}
+
+.pvh-logo {
+  width: 100px; /* Adjust width as needed */
+  height: auto; /* Maintain aspect ratio */
+  margin-bottom: 10px; /* Space between logo and title */
+}
+
+.report-title {
+  font-size: 24px; /* Adjust font size as needed */
+  margin: 0; /* Remove default margin */
+  /* Additional styling for the title */
+}
   .carousel-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  position: relative;
+  height: 80vh;
+  margin: auto;
+}
+
+  .video-container {
+    width: 100%; /* Full width of the container */
+    height: 70vh; /* Fixed height, adjust as needed */
+    overflow: hidden; /* Hide overflow to maintain the size */
+    position: relative; /* For absolute positioning of the video */
   }
-  .carousel-button {
-    padding: 0.5rem 1rem;
-    margin: 0 1rem;
-    cursor: pointer;
+
+  .carousel-video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* Cover the container completely */
+    position: absolute;
+    top: 0;
+    left: 0;
   }
-  .carousel-image {
-    max-width: 100%;
-    height: auto;
-  }
-  .brand-info {
-    text-align: center;
-  }
+
+.carousel-button {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: white;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  z-index: 2;
+  border: none;
+  cursor: pointer;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+}
+
+.carousel-button.previous {
+  left: 10px;
+}
+
+.carousel-button.next {
+  right: 10px;
+}
+
 </style>
