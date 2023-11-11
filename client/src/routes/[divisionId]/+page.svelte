4a99b7441999a6619983ded6a0c884f8fabc7e3d
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-11-11 19:20:55 +0100

    Finalized basic layout for division overview pages

--- client/src/routes/[divisionId]/+page.svelte
+++ client/src/routes/[divisionId]/+page.svelte
@@ -13,13 +13,19 @@
     $: currentDivision = menuList.find(division => division.Div === divisionId) || null;
     
     // Assuming each division object in your menuList has a property 'img' that contains the path to the image
-    $: backgroundImage = currentDivision ? currentDivision.img : '';
+    $: headerImage = currentDivision ? currentDivision.img : '';
     $: themeClass = currentDivision ? currentDivision.theme : 'theme-default';
     $: divisionName = currentDivision ? currentDivision.name.toUpperCase() : '';
+
+    let soldOutStyleOptionsCount = 0;
+
   </script>
   
   {#if currentDivision}
-    <div class="container mx-auto p-4 {themeClass}" style="background-image: url({backgroundImage}); background-size: cover; background-position: center;">
+    <div class="container mx-auto p-4 {themeClass}">
+      <header style="width: 100%; height: 200px; background-image: url({headerImage}); background-size: cover; background-position: center;">
+        <!-- No img tag needed -->
+      </header>
       <div class="flex flex-row justify-center space-x-4">
         <div class="w-1/2">
           <!-- Column 1 -->
@@ -33,10 +39,36 @@
               <p><ProgressBar percentage={90} completed={1256} total={1398} /></p>
               <p><ProgressBar percentage={90} completed={1256} total={1398} /></p>
               <p><ProgressBar percentage={90} completed={1256} total={1398} /></p>
+              <div class="flex justify-center mt-4">
+                <button class="btn-th-detailed-report">
+                  VIEW DETAILED REPORT
+                </button>
+              </div>
+            </div>
+            <div class="p-4">
+            </div>
+          </div>
+          <div class="card-th mb-4">
+            <div class="p-4">
+              <p class="font-bold">MISSING STYLE OPTIONS: {soldOutStyleOptionsCount}</p>
+            </div>
+          </div>
+          <div class="card-th mb-4">
+            <div class="p-4">
+              <p class="font-bold">PARTIALLY COMPLETED STYLE OPTIONS: {soldOutStyleOptionsCount}</p>
+            </div>
+          </div>
+          <div class="card-th mb-4">
+            <div class="p-4">
+              <p class="font-bold">CANCELLED STYLE OPTIONS: {soldOutStyleOptionsCount}</p>
+            </div>
+          </div>
+          <div class="card-th mb-4">
+            <div class="p-4">
+              <p class="font-bold">SOLD OUT STYLE OPTIONS: {soldOutStyleOptionsCount}</p>
             </div>
           </div>
         </div>
-  
         <div class="w-1/2">
           <!-- Column 2 -->
           <div class="card-th mb-4">
@@ -47,11 +79,16 @@
               <p>{currentDivision.looksCompleted} OF {currentDivision.looksTotal} COMPLETE</p>
               <p><ProgressBar percentage={90} completed={1256} total={1398} /></p>
               <p><ProgressBar percentage={90} completed={1256} total={1398} /></p>
+              <div class="flex justify-center mt-4">
+                <button class="btn-th-detailed-report">
+                  VIEW DETAILED REPORT
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+</div>
   {:else}
     <p>No division found for ID: {divisionId}</p>
   {/if}
