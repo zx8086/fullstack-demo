d582669870f630fbe956e741ea6232d08fbc84a9
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-11-11 01:55:14 +0100

    completed base styling

--- client/src/components/dataLoading.svelte
+++ client/src/components/dataLoading.svelte
@@ -1,20 +1,48 @@
 <script lang="ts">
-</script>
-
-<div class="loader">
+    // Define a variable to determine the theme (TH or CK)
+    let theme = 'TH'; // You can change this dynamically based on your logic
+  
+    // Function to determine the appropriate loader class based on the theme
+    function getLoaderClass() {
+      if (theme === 'TH') {
+        return 'loader-th'; // Use loader-th class for TH theme
+      } else if (theme === 'CK') {
+        return 'loader-ck'; // Use loader-ck class for CK theme
+      }
+    }
+  </script>
+  
+  <div class={getLoaderClass()}>
     <img src="/Spinner-1.1s-211px.svg" alt="Spinner">
-</div>
+  </div>
 
 <style>
-    .loader {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        display: grid;
-        place-items: center;
-        background-color: white;
-        z-index: 1000;
+    .loader-th {
+      /* Styles for TH theme loader */
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      display: grid;
+      place-items: center;
+      background-color: white;
+      z-index: 1000;
+      /* Add other TH-specific styles here */
     }
-</style>x
+  
+    .loader-ck {
+      /* Styles for CK theme loader */
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      display: grid;
+      place-items: center;
+      background-color: blue; /* CK-specific background color */
+      color: white; /* CK-specific text color */
+      z-index: 1000;
+      /* Add other CK-specific styles here */
+    }
+  </style>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,51 @@
+// tailwind.config.js
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'tommy': {
+          'white': '#ffffff', // Text color
+          'gray': '#969696', // Text color
+          'red': '#cc0c2f', // Text color
+          'orange': '#cb1017', // Text color, darker variation of red
+          'blue': '#0000ee', // Text color
+          'purple': '#551a8b', // Text color
+          'dark-blue': '#00174f', // Text color, darker variation of blue
+          'darker-blue': '#000c2d', // Text color, darkest blue variation
+          'light-gray': '#5a5c66', // Text color, lighter gray
+          'black': '#000000', // Text color
+          
+          'background-lightest': '#ffffff', // Background color, lightest
+          'background-lighter': '#f4f4f4', // Background color, lighter
+          'background-light-gray': '#e5e5e5', // Background color, light gray
+          'background-cream': '#e8e4dc', // Background color, cream
+
+          'background-medium-gray': '#cccccc', // Background color, medium gray
+          'background-medium-gray-two': '#cbcbcb', // Background color, similar to above, slight variation
+          'background-slate-gray': '#c8c8c8', // Background color, slate gray
+          'background-cool-gray': '#c0c0c0', // Background color, cool gray
+
+          'background-dark-gray': '#b5b5b5', // Background color, darker gray
+          'background-darker-gray': '#b3b3b3', // Background color, darker than the above gray
+          'background-navy': '#00174fcc', // Background color, navy with some transparency
+          'background-deep-blue': '#000c2d', // Background color, deep blue
+
+          'fill-lightest': '#ffffff', // Fill color, lightest
+          'fill-off-white': '#fffffe', // Fill color, off-white
+          'fill-red': '#cc0d2e', // Fill color, red
+          'fill-dark-blue': '#00174f', // Fill color, dark blue
+          'fill-darker-blue': '#000c2d', // Fill color, darkest blue
+          'fill-black': '#000000', // Fill color, black
+
+          'border-white': '#ffffff', // Border color, white
+          'border-red': '#cb1017', // Border color, red
+          'border-dark-blue': '#000c2d', // Border color, dark blue
+        }
+      }
+    }
   },
   plugins: []
 };
