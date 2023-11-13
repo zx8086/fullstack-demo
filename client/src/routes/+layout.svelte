64576ae8a8b9ba6b6d4cf07c8c5dabb314558c6d
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-11-13 02:14:19 +0100

    Moved Elastic APM Script to app html

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-    import "../app.css";
-    import { onMount } from 'svelte';
-    import { themeStore } from '../stores/themeStore';
-    import { menuListStore, setMenuList } from '../stores/menuList';
-    import { writable } from 'svelte/store';
-    import { Sidebar } from 'svelte-sidebar-menu';
+  import "../app.css";
+  import { onMount } from 'svelte';
+  import { themeStore } from '../stores/themeStore';
+  import { menuListStore, setMenuList } from '../stores/menuList';
+  import { writable } from 'svelte/store';
+  import { Sidebar } from 'svelte-sidebar-menu';
 
-    let siteName = "Home";
-    let headerClass = "bg-white py-3 px-10 items-center text-black py-4 border-b-2";
-    let asideClass = "absolute w-auto h-full bg-white pt-8 border-r-2 shadow-lg";
-    let navClass = "bg-white p-7 text-base";
+  let siteName = "Home";
+  let headerClass = "${themeClass} animate-pulse py-3 pl-3";
+  let asideClass = "${themeClass} bg-transparent absolute w-auto";
+  let navClass = "${themeClass} uppercase pl-5 pr-10 leading-10";
 
   onMount(() => {
     // This will run only in the browser, after the component has mounted
@@ -17,34 +17,6 @@
     if (storedTheme) {
       themeStore.set(storedTheme);
     }
-    
-      const script = document.createElement('script');
-      script.src = "/elastic-apm-rum.umd.js";
-      // script.src = "https://e6621070.cloudfront.localhost.localstack.cloud:4566/elastic-apm-rum.umd.js";
-      script.onload = () => {
-        console.log('Elastic APM RUM script loaded.');
-        if (window.elasticApm) {
-          try {
-            window.elasticApm.init({
-              serviceName: 'Svelte-Frontend',
-              serverUrl: 'https://apm.siobytes.com',
-              serviceVersion: '1.0.0',
-              environment: process.env.NODE_ENV || 'development',
-              logLevel: 'trace',
-              breakdownMetrics: true,
-            });
-            console.log('Elastic APM initialized.');
-          } catch (e) {
-            console.error('Error initializing Elastic APM RUM:', e);
-          }
-        } else {
-          console.error('Elastic APM RUM script loaded but window.elasticApm is undefined.');
-        }
-      };
-      script.onerror = (event) => {
-        console.error('Elastic APM RUM script failed to load.', event);
-      };
-    document.head.appendChild(script);
 
     // Set initial theme class on body
     updateBodyClass($themeStore);
@@ -54,7 +26,6 @@
 
     return () => {
       // Cleanup: remove the script if needed, and clean up any classes added to body
-      script.remove();
       document.body.classList.remove(themeClass);
     };
   });
@@ -72,7 +43,7 @@
 
 <Sidebar lists={$menuListStore} {siteName} {headerClass} {asideClass} {navClass}/>
 <main class="container mx-auto p-24 ${themeClass}">
-  <h1 class="text-3xl">
-    <!-- <slot></slot> -->
-  </h1>
+<h1 class="text-3xl">
+  <slot></slot>
+</h1>
 </main>
